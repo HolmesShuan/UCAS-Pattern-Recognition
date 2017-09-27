e30d39c16d2178b43d7e17a1f9ec3dc1eeb379f7
Shuan <ShuanHolmes@Outlook.com>
2017-09-27 23:47:34 +0800

    Add files via upload

---
+++ Bayesian Decision/QDF.m
@@ -0,0 +1,4 @@
+function g = QDF( x , W, w, w0)
+    g = x'*W*x + w'*x + w0;
+end
+
---
+++ Bayesian Decision/decision_function.m
@@ -0,0 +1,18 @@
+%%
+% Info: Bayesian Decision
+% Auth: Shuan
+% Env : MatLab2016b and upper
+%%
+function [W,w,w_0] = decision_function(X, P_w, cases)
+    if cases == 1
+    elseif cases == 2    
+    else
+        Cov_Matrix = cov(X);
+        mean_vector = mean(X);
+        mean_vector = mean_vector';
+        Sigma_inv = pinv(Cov_Matrix);
+        W = Sigma_inv/-2;
+        w = Sigma_inv*mean_vector;
+        w_0 = mean_vector'*Sigma_inv*mean_vector/-2 + log(det(Cov_Matrix))/-2 + log(P_w);
+    end
+end
